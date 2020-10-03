20a69e2f7971601220cb3a7c6488b8dfee0c3d4c
Cason White <whitecas18@students.ecu.edu>
2020-10-03 10:05:56 -0400

    delete extraneous files and add gitignore

--- canvas_api/pythonexample.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-headers = {
-    'Authorization': 'Bearer 13605~tyg7VFRxKn0BmCdQUK5gzgte5Sy8H4lgYCwFfPz3Bbb5sU6k3nD5l46WORwS4AEF',
-}
-
-response = requests.get('https://canvas.instructure.com/api/v1/courses', headers=headers)
-
-print(response.content)
